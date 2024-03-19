4c76bedb7a2bd6acfb88ba7ada0c6b0f34d24ef4
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-19 15:43:58 +0100

    <feat>[controller][main][get_apparent_matrix]:<adding main_loop from view into the main of controller, and adding a methods that return the apparent_matrix from model>

--- src/Controller.py
+++ src/Controller.py
@@ -9,3 +9,15 @@ class Controller:
     def set_position(self, x, y):
         '''Sets the position of the controller'''
         self.model.set_position(x, y)
+
+
+    def main(self):
+        '''Main function of the controller'''
+        self.view.main_loop()
+
+    def get_apparent_matrix(self):
+        return self.model.get_apparent_matrix()
+
+if __name__ == "__main__":
+    controller = Controller()
+    controller.main()
